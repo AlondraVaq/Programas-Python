77e240b6f7b5152cbe77c9a4bb5660a1807245a0
AlondraVaq <alondramvd1011@gmail.com>
2024-08-21 13:15:39 -0600

    Actividad 2

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 - Correo: alondramvd1011@gmail.com
 
 ## ACTIVIDAD 1
-- Programa Python 1
-- Programa Python 2
+- Repositorio
+
+## ACTIVIDAD 2
+- p01-hola-mundo.py
+- p02-area-circulo.py
+- p03-area-triangulo.py
+- p04-paga-trabajador.py
---
+++ p01-hola-mundo.py
@@ -0,0 +1,17 @@
+# p01-hola-mundo - Recibe datos de usuario y los imprime en pantalla
+
+nombre = input("Dame tu nombre ?")
+edad = input ("Edad ?")
+peso = float(input("Peso ?"))
+
+print(nombre)
+print(edad)
+print(peso)  
+
+print(f"{nombre} bienvenido a Python, tu edades {edad} tu peso {peso}")
+
+print(type(nombre))
+print(type(edad))
+print(type(peso))
+
+
